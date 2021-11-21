98265fa7f2f0d87e9592160ba15c7d8520fe674f
YajunWang <wangyajun@hetao101.com>
2021-11-21 16:14:11 +0800

    feat: eg struct area

--- p_struct/src/main.rs
+++ p_struct/src/main.rs
@@ -1,5 +1,14 @@
+use std::sync::mpsc::Receiver;
+
 fn main() {
 
+//    runP1();
+
+    runP2();
+
+}
+
+fn runP1() {
     let mut user1 = User {
         email: String::from("ayac3j@gmail.com"),
         username: String::from("aya"),
@@ -23,9 +32,7 @@ fn main() {
     user1.email = String::from("a@gmail.com");
     println!("Hello, world! {:?}", user3);
     println!("Hello, world! {:?}", user1.email);
-
 }
-
 #[derive(Debug)]
 struct User {
     username: String,
@@ -36,4 +43,35 @@ struct User {
 
 fn build_user(email: String, username: String) -> User {
     User { username: username, email: email, sign_in_count: 1, active: false }
+}
+
+fn runP2 () {
+    let width1 = 30;
+    let height1 = 50;
+    let area_value1 = area(width1, height1);
+    let area_value2 = area_by_tupl((width1, height1));
+    let scale = 2;
+    let rect1 = Rectangle { width: dbg!(30 * scale), height: 50 };
+    println!("rect struct {:?}", rect1);
+    let area_value = area_by_struct(rect1);
+    println!("The area of the rectangle is {} square pixels", area_value);
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area_by_tupl (dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area_by_struct (rect: Rectangle) -> u32 {
+    rect.width * rect.height
 }
