5cb178d3d0a85371899e4ef0cfc225422f5d3b41
YajunWang <wangyajun@hetao101.com>
2021-11-21 14:59:42 +0800

    feat: run p_slice

--- p_slice/src/main.rs
+++ p_slice/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world");
+    let world = first_word(&s[..]);
+    println!("Hello, world! {}", world);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
 fn main() {
-    let s = String::from("hello world");
-    let world = first_word(&s[..]);
-    println!("Hello, world! {}", world);
-}
-
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-    &s[..]
+    println!("Hello, world!");
 }
