73bc9a87b6df3fdb0b757d6abb7630311f407878
YajunWang <wangyajun@hetao101.com>
2021-11-30 15:11:52 +0800

    feat: print

--- p_print/src/main.rs
+++ p_print/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("{} days", 31i64);
+    // 31 days
+
+    println!("{} days", 31);
+    // 31 days
+
+    // 作为索引使用
+    println!("{0}, this is {1}, this is {2}", "A", "B", "C");
+    // A, this is B, this is C
+
+    // 别名使用
+    println!("{a}, {b}, {c}",
+        a="A",
+        b="B",
+        c="C",
+    );
+    // A, B, C
+
+    println!("{} of {:b}", 1, 2);
+    // 1 of 10
+
+    println!("{number:>width$}", number=1, width=10);
+    //         1
+
+    println!("{number:#>width$}", number=1, width=10);
+    // #########1
+    
 }
