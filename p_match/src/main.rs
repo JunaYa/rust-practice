0d34515557234cd3eda9bc6f8a0c1b1fc1ef8031
YajunWang <wangyajun@hetao101.com>
2021-12-02 15:16:11 +0800

    feat: 匹配命名变量

--- p_match/src/main.rs
+++ p_match/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     println!("Hello, world!");
 
     matchFn1();
+
+    matchFn2();
 }
 
 // 匹配字面值
@@ -13,6 +15,17 @@ fn matchFn1 () {
         2 => println!("two"),
         3 => println!("three"),
         _ => println!("None"),
+    }
+}
 
+// 匹配命名变量
+fn matchFn2 () {
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched y={:?}", y),
+        _ => println!("none match"),
     }
 }
