4170d5c1189c922b0ba7fc61f4f74429835666af
YajunWang <wangyajun@hetao101.com>
2021-12-24 18:59:14 +0800

    feat: From & Into

--- p_conversion/src/main.rs
+++ p_conversion/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
+
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    let int = 5;
+    let num1: Number = int.into();
+    println!("My number2 is {:?}", num1);
+}
+
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
 }
