51b0b56fab33603bde80f8f93e45b266dc469ba1
YajunWang <wangyajun@hetao101.com>
2021-12-28 16:44:05 +0800

    feat: add if else

--- p_flow_of_controll/src/main.rs
+++ p_flow_of_controll/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+    p_if_else();
+}
+
+fn p_if_else () {
+    let n = -5;
+    if n < 0 {
+        println!("{} is negative", n);
+    } else if n > 0 {
+        println!("{} is positive", n);
+    } else {
+        println!("{} is zero", n);
+    }
 }
