c37ded0d063ce184efda9eae2f11eac6d97dee9c
YajunWang <wangyajun@hetao101.com>
2022-01-07 15:05:09 +0800

    feat: add capturing

--- p_function_closures/src/main.rs
+++ p_function_closures/src/main.rs
@@ -15,4 +15,49 @@ fn main() {
     let one = || i;
 
     println!("one {}", one());
+
+    capturing();
+}
+
+fn capturing () {
+    use std::mem;
+
+    let color = String::from("green");
+    let print = || println!("color: {}", color);
+    print();
+
+    let _reborrow = &color;
+    print();
+
+    let _color_move = color;
+    let mut count = 0;
+
+    let mut inc = || {
+        count += 1;
+        println!("count {}", count);
+    };
+
+    inc();
+
+    inc();
+
+    let _count_reborrow = &mut count;
+
+    let movable = Box::new(2);
+
+    let consume = || {
+        println!("movable - {}", movable);
+        mem::drop(movable);
+    };
+
+    consume();
+
+    // consume();
+
+    let haystack = vec![1, 2, 3];
+    let contains = move |needle| haystack.contains(needle);
+
+    println!("{}", contains(&3));
+    println!("{}", contains(&1));
+    
 }
