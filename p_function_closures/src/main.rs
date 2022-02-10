7267c446fb1f6758d693141fd9059006f449b4d6
YajunWang <wangyajun@hetao101.com>
2022-02-10 17:49:34 +0800

    feat: update iterators

--- p_function_closures/src/main.rs
+++ p_function_closures/src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 fn main() {
    
     init();
@@ -9,14 +11,23 @@ fn main() {
 
     let vec1 = vec![1, 2, 3];
     let vec2 = vec![4, 5, 6];
+    // println!("2 in vec1: {}", vec1.iter()     .any(|&x| x == 2));
+    // println!("2 in vec2: {}", vec2.into_iter().any(| x| x == 2));
 
-    println!("2 in vec1: {}", vec1.iter()     .any(|&x| x == 2));
-    println!("2 in vec2: {}", vec2.into_iter().any(| x| x == 2));
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+    let mut iter = vec1.iter();
+    let mut into_iter = vec2.into_iter();
+
+    println!("2 in vec1: {:?}", iter     .find(|&&x| x == 2));
+    println!("2 in vec2: {:?}", into_iter.find(| &x| x == 2));
 
     let arr1 = [1, 2, 3];
     let arr2 = [4, 5, 6];
-    println!("2 in array1: {}", arr1.iter()     .any(|&x| x == 2));
-    println!("2 in array2: {}", arr2.into_iter().any(| x| x == 2));
+    // println!("2 in array1: {}", arr1.iter()     .any(|&x| x == 2));
+    // println!("2 in array2: {}", arr2.into_iter().any(| x| x == 2));
+    println!("2 in array1: {:?}", arr1.iter()     .find(|&&x| x == 2));
+    println!("2 in array2: {:?}", arr2.into_iter().find(|&x| x == 2));
 }
 
 fn init () {
