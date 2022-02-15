56381113c8e3a23adee0abee948d0545cbc296f0
YajunWang <wangyajun@hetao101.com>
2022-02-15 16:50:34 +0800

    feat: update crate

---
+++ p_attributes/src/lib.rs
@@ -0,0 +1,14 @@
+#![crate_type = "lib"]
+#![crate_name = "rary"]
+pub fn public_function () {
+	println!("called rary`s public_function");
+}
+
+fn private_function () {
+	println!("called rary`s private_function");
+}
+
+pub fn indirect_function () {
+	println!("called rary`s indirect_function");
+	private_function();
+}
