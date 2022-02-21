807c218ccd97dccd824918ddaa48b1e0900e9429
YajunWang <wangyajun@hetao101.com>
2022-02-21 17:24:41 +0800

    feat: update scope;

--- p_scope_rule/src/main.rs
+++ p_scope_rule/src/main.rs
@@ -11,6 +11,20 @@ fn main() {
         borrow_i32(ref_to_i32);
     }
     eat_box_i32(boxed_i32);
+
+    println!("-----------------------");
+
+    let immutabook = Book {
+        author: "aya",
+        title: "Good Book",
+        year: 2021,
+    };
+
+    let mut mutabook = immutabook;
+    borrow_book(&immutabook);
+    borrow_book(&mutabook);
+    new_edition(&mut mutabook);
+    // new_edition(&mut immutabook);
 }
 
 fn eat_box_i32 (boxed_i32: Box<i32>) {
@@ -19,4 +33,20 @@ fn eat_box_i32 (boxed_i32: Box<i32>) {
 
 fn borrow_i32 (borrowed_i32: &i32) {
     println!("borrowed_i32 is {}", borrowed_i32);
+}
+
+#[derive(Clone, Copy)]
+struct Book {
+    author: &'static str,
+    title: &'static str,
+    year: i32,
+}
+
+fn borrow_book (book: &Book) {
+    println!("borrow book title :{}, author: {}", book.title, book.author);
+}
+
+fn new_edition (book: &mut Book) {
+    book.year = 2022;
+    println!("book edition year is {}", book.year);
 }
