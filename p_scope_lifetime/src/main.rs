1779faafd635119c0216c1bcf3c7a54b2510125c
YajunWang <wangyajun@hetao101.com>
2022-02-22 15:25:24 +0800

    feat: update life

--- p_scope_lifetime/src/main.rs
+++ p_scope_lifetime/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+
+    let (x, y) = (2, 3);
+    print_refs(&x, &y);
+
+    println!("out x is {}", &x);
+
+    borrow_error();
+}
+
+fn print_refs<'a, 'b> (x: &'a i32, y: &'b i32) {
+    println!("x is {} y is {}", x, y);
+}
+
+fn borrow_error<'a> () {
+    let _x = 2;
+    // let y: &'a i32 = &_x;
 }
