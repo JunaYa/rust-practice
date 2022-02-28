804baa0815598cdac2fb065244d286b2caa3470c
YajunWang <wangyajun@hetao101.com>
2022-02-28 14:07:07 +0800

    feat: update string

--- p_string/src/main.rs
+++ p_string/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+    let my_name =  "Pascal";
+    // great(my_name);
+
+    // slice
+    let s = String::from("hello");
+    let slice1 = &s[0..2];
+    let slice2 = &s[2..s.len()];
+    let slice = &s[..];
+}
+
+fn great(name: String) {
+    println!("string is {}", name);
 }
