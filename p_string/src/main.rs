54e0ec2fcdcf159777d3a09dfb4156ab1dab3eb5
YajunWang <wangyajun@hetao101.com>
2022-03-01 11:57:19 +0800

    feat: add string

--- p_string/src/main.rs
+++ p_string/src/main.rs
@@ -8,8 +8,14 @@ fn main() {
     let slice1 = &s[0..2];
     let slice2 = &s[2..s.len()];
     let slice = &s[..];
+    let arr: [i32; 2] = [1, 2];
+    display_array(arr);
 }
 
-fn great(name: String) {
+fn great(name: &String) {
     println!("string is {}", name);
+}
+
+fn display_array <T: std::fmt::Debug, const N: usize> (arr: [T; N]) {
+    println!("{:?}", arr);
 }
